2587c4f460433a06298bcb5a6e645861bd100bb7
Jesper Lundbäck <68069660+Jesper1990@users.noreply.github.com>
2020-10-21 17:14:10 +0200

    Test adding test2

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("test");
+		System.out.println("fan händer");
+		System.out.println("test2");
 	}
 
 }
