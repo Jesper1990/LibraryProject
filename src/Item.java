be5b44cc3ed1ae844f0ba34f724cd278b1ec7a95
Jesper Lundbäck <68069660+Jesper1990@users.noreply.github.com>
2020-10-29 16:33:40 +0100

    created methods for serialising files

--- src/Item.java
+++ src/Item.java
@@ -1,5 +1,6 @@
+import java.io.Serializable;
 
-public class Item {
+public class Item implements Serializable{
 
 	protected int productId;
 	protected String title;
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.Scanner;
 
 public class Main {
@@ -5,6 +9,7 @@ public class Main {
 	private static Scanner scanner = new Scanner(System.in);
 	private static boolean isRunning;
 	public static Library lib = new Library();
+	private static String fileName = "library.txt";
 
 
 	public static void main(String[] args) {
@@ -17,9 +22,15 @@ public class Main {
 			String str = scanner.next();
 			if(str.equals("b")) {
 				addBook();
+				System.out.println("Sucessfully registered: ");
 			}
 			else if (str.equals("m")) {
 				addMovie();
+				System.out.println("Wanna register a book (b) or movie (m) ?");
+			} else if (str.equals("save")) {
+				addList();
+			} else if (str.equals("read")) {
+				readList();
 			}
 		}
 
@@ -69,6 +80,34 @@ public class Main {
 		lib.addMovie(m);
 		System.out.println(lib);
 
+	}
+	
+	public static void addList() {
+		FileOutputStream fos = null;
+		ObjectOutputStream oos = null;
+		try {
+			fos = new FileOutputStream(fileName);
+			oos = new ObjectOutputStream(fos);
+			oos.writeObject(lib);
+			oos.close();
+		} catch(Exception e ) {
+			e.printStackTrace();
+			return;
+		}
+	}
+	public static void readList() {
+		FileInputStream fis = null;
+		ObjectInputStream ois = null;
+		try {
+			fis = new FileInputStream(fileName);
+			ois = new ObjectInputStream(fis);
+			lib = (Library) ois.readObject();
+			System.out.println(lib);
+			ois.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return;
+		}
 	}
 }	
 /* Vad skall göras? = Ett förenklat lagersystem som skall användas utav ett bibliotek.
