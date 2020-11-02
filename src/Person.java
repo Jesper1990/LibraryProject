a6150ea5641ec83cbf172d3a10cdf5e476f0b3da
Jesper Lundbäck <68069660+Jesper1990@users.noreply.github.com>
2020-11-02 16:47:53 +0100

    La till en klass för Person

---
+++ src/Person.java
@@ -0,0 +1,20 @@
+import java.io.Serializable;
+
+public class Person implements Serializable {
+	
+	protected String name;
+	protected String phoneNum;
+	
+	public Person() {
+		name = null;
+		phoneNum = null;
+	}
+	public Person(String name, String phoneNum) {
+		this.name = name;
+		this.phoneNum = phoneNum;
+	}
+	@Override
+	public String toString() {
+		return name + " " + phoneNum;
+	}
+}
